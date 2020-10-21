799fb2d22597c3f5783620563be7b8eaae344018
dzmitry-liashenka <mitrybakovich@gmail.com>
2020-10-21 17:22:21 +0300

    HW03 optional 5.2 Junit tests

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,58 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.time.LocalDateTime.now;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MealTestData {
+    public static final int MEAL_ID = 3;
+    public static final int MEAL_ID_UPDATE = 2;
+
+    public static final Meal meal4 = new Meal(4,
+            LocalDateTime.of(2020, 10, 20, 07, 15, 00, 000000),
+            "frühstückNachsteTag", 1111);
+
+    public static final Meal meal1 = new Meal(3,
+            LocalDateTime.of(2020, 10, 19, 18, 00, 00, 000000),
+            "abendessen", 500);
+
+    public static final Meal meal2 = new Meal(2,
+            LocalDateTime.of(2020, 10, 19, 12, 00, 00, 000000),
+            "mittag", 1500);
+
+    public static final Meal meal3 = new Meal(1,
+            LocalDateTime.of(2020, 10, 19, 07, 00, 00, 000000),
+            "frühstück", 1000);
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    private static void assertMatch(Iterable<Meal> actual, List<Meal> expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Meal getNew() {
+        return new Meal(null, now(), "NewMeal", 1234);
+    }
+
+    public static Meal getUpdated() {
+        meal2.setDescription("updated");
+        meal2.setCalories(321);
+        return meal2;
+    }
+
+    public static Meal[] getExpectedBetween() {
+        Meal[] meals = {meal1, meal2, meal3};
+        return meals;
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,79 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.assertThrows;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void get() {
+        Meal meal = service.get(MEAL_ID, USER_ID);
+        assertMatch(meal1, meal);
+    }
+
+    @Test
+    public void delete() {
+        service.delete(MEAL_ID, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(MEAL_ID, USER_ID));
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+        List<Meal> actual = service.getBetweenInclusive(LocalDate.of(2020, 10, 18),
+                LocalDate.of(2020, 10, 19), USER_ID);
+        assertMatch(actual, getExpectedBetween());
+    }
+
+    @Test
+    public void getAll() {
+        List<Meal> all = service.getAll(USER_ID);
+        assertMatch(all,meal4, meal1, meal2, meal3);
+    }
+
+    @Test
+    public void update() {
+        Meal updated = getUpdated();
+        service.update(updated, USER_ID);
+        assertMatch(service.get(MEAL_ID_UPDATE, USER_ID), updated);
+    }
+
+    @Test
+    public void create() {
+        Meal newMeal = getNew();
+        Meal created = service.create(newMeal, USER_ID);
+        Integer newId = created.getId();
+        newMeal.setId(newId);
+        assertMatch(created, newMeal);
+        assertMatch(service.get(newId, USER_ID), newMeal);
+    }
+
+
+}
