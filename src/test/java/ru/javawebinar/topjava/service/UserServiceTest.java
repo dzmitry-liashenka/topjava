15b9ee1c6216cb8e2c65ff1c221bfddca63378b5
liashenka <dzmitry_liashenka@yahoo.com>
2020-11-02 15:11:57 +0100

    HW05 2. profiles for jdbc, jpa, datajpa

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -28,7 +28,8 @@ import static ru.javawebinar.topjava.UserTestData.*;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+//@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+@ActiveProfiles(profiles = {"postgres", "datajpa"})
 public class UserServiceTest {
 
     @Autowired
