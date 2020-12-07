37c5c6fd42c5f1915d6229b99a504950e5a479be
liashenka <dzmitry_liashenka@yahoo.com>
2020-12-07 12:55:44 +0100

    [HW09] 3. fixed MealRestControllerTest, added getUnAuth() test

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -40,9 +40,9 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     void create() {
-        Meal created = service.create(getNew(), USER_ID);
+        Meal created = service.create(getNewMeal(), USER_ID);
         int newId = created.id();
-        Meal newMeal = getNew();
+        Meal newMeal = getNewMeal();
         newMeal.setId(newId);
         MEAL_MATCHER.assertMatch(created, newMeal);
         MEAL_MATCHER.assertMatch(service.get(newId, USER_ID), newMeal);
@@ -72,9 +72,9 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     void update() {
-        Meal updated = getUpdated();
+        Meal updated = getUpdatedMeal();
         service.update(updated, USER_ID);
-        MEAL_MATCHER.assertMatch(service.get(MEAL1_ID, USER_ID), getUpdated());
+        MEAL_MATCHER.assertMatch(service.get(MEAL1_ID, USER_ID), getUpdatedMeal());
     }
 
     @Test
