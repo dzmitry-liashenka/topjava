bd9317a807a14f9378df6e208e92ee30266371bc
liashenka <dzmitry_liashenka@yahoo.com>
2020-12-16 11:48:46 +0100

    [HW10] 2. test requests for rest with curl Validation for rest

--- config/curl.md
+++ config/curl.md
@@ -10,6 +10,12 @@
 #### register Users
 `curl -s -i -X POST -d '{"name":"New User","email":"test@mail.ru","password":"test-password"}' -H 'Content-Type:application/json;charset=UTF-8' http://localhost:8080/topjava/rest/profile/register`
 
+#### register User with duplicated email
+`curl -s -i -X POST -d '{"name":"New User","email":"user@yandex.ru","password":"test-password"}' -H 'Content-Type:application/json;charset=UTF-8' http://localhost:8080/topjava/rest/profile/register/ --user admin@gmail.com:admin`
+
+#### update User with duplicated email
+`curl -s -i -X PUT -d '{"name":"New User","email":"user@yandex.ru","password":"test-password"}' -H 'Content-Type:application/json;charset=UTF-8' http://localhost:8080/topjava/rest/profile --user admin@gmail.com:admin`
+
 #### get Profile
 `curl -s http://localhost:8080/topjava/rest/profile --user test@mail.ru:test-password`
 
@@ -33,6 +39,12 @@
 
 #### update Meals
 `curl -s -X PUT -d '{"dateTime":"2020-01-30T07:00", "description":"Updated breakfast", "calories":200}' -H 'Content-Type: application/json' http://localhost:8080/topjava/rest/profile/meals/100003 --user user@yandex.ru:password`
+
+#### create Meal with duplicate datetime
+`curl -s -X POST -d '{"dateTime":"2020-01-31T21:00", "description":"meal with duplicate datetime", "calories":200}' -H 'Content-Type: application/json' http://localhost:8080/topjava/rest/profile/meals --user admin@gmail.com:admin`
+
+#### update Meal with not valid field
+`curl -s -X PUT -d '{"dateTime":"", "description":"not valid meal", "calories":200}' -H 'Content-Type: application/json' http://localhost:8080/topjava/rest/profile/meals/100010 --user admin@gmail.com:admin`
 
 #### validate with Error
 `curl -s -X POST -d '{}' -H 'Content-Type: application/json' http://localhost:8080/topjava/rest/admin/users --user admin@gmail.com:admin`
--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -1,13 +1,16 @@
 package ru.javawebinar.topjava.web.user;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
@@ -30,7 +33,7 @@ public class ProfileRestController extends AbstractUserController {
 
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<User> register(@RequestBody UserTo userTo) {
+    public ResponseEntity<User> register(@Valid @RequestBody UserTo userTo) {
         User created = super.create(userTo);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(REST_URL).build().toUri();
@@ -39,8 +42,12 @@ public class ProfileRestController extends AbstractUserController {
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@RequestBody UserTo userTo) {
-        super.update(userTo, authUserId());
+    public void update(@Valid @RequestBody UserTo userTo) {
+//        try {
+            super.update(userTo, authUserId());
+//        } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+//            throw new DataIntegrityViolationException("User with this email already present in application");
+//        }
     }
 
     @GetMapping("/text")
