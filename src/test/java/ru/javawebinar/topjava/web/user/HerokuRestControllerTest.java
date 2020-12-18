ac2e1c889eb5fc2cff8d9aea7625fb4b3ec83aaa
liashenka <dzmitry_liashenka@yahoo.com>
2020-12-18 22:34:42 +0100

    11_16_small_fix

--- src/test/java/ru/javawebinar/topjava/web/user/HerokuRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/HerokuRestControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.util.exception.ErrorType;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 
@@ -35,7 +34,7 @@ class HerokuRestControllerTest extends AbstractControllerTest {
         perform(MockMvcRequestBuilders.put(REST_URL + USER_ID)
                 .contentType(MediaType.APPLICATION_JSON)
                 .with(userHttpBasic(admin))
-                .content(UserTestData.jsonWithPassword(user, "password")))
+                .content(jsonWithPassword(user, "password")))
                 .andExpect(errorType(ErrorType.VALIDATION_ERROR))
                 .andExpect(detailMessage(EXCEPTION_UPDATE_RESTRICTION))
                 .andExpect(status().isUnprocessableEntity());
